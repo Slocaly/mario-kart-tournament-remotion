a41ef2f7ee8faa96328e2d9320c48aaf9e1f6747
IDOUX Etienne <etienne.idoux@gmail.com>
2023-03-02 22:09:43 +0100

    🥊 Create versus video

---
+++ utils/people.ts
@@ -0,0 +1,24 @@
+import JeremiePicard from "../public/people/JeremiePicard.jpg";
+import ClemencePiteau from "../public/people/ClemencePiteau.jpg";
+import AnthonyDonnet from "../public/people/AnthonyDonnet.jpg";
+import LoicDuchet from '../public/people/LoicDuchet.jpg';
+import LucasAudart from '../public/people/LucasAudart.jpg';
+import MathieuMure from '../public/people/MathieuMure.jpg';
+import MickaelAlves from '../public/people/MickaelAlves.jpg';
+import MoustaphaAgack from '../public/people/MoustaphaAgack.jpg';
+import OlivierPerez from '../public/people/OlivierPerez.jpg';
+import PierreReynaud from '../public/people/PierreReynaud.jpg';
+
+
+export {
+  JeremiePicard,
+  ClemencePiteau,
+  AnthonyDonnet,
+  LoicDuchet,
+  LucasAudart,
+  MathieuMure,
+  MickaelAlves,
+  MoustaphaAgack,
+  OlivierPerez,
+  PierreReynaud
+}
