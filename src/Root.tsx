671630f6a869541a6bd79f6f49c2fcabb31b0d46
Audart Lucas <lucas.audart@ocea-sb.com>
2023-03-13 10:19:54 +0100

    🛠 Add several build npm script

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,38 +1,37 @@
-/* eslint-disable camelcase */
-import { Composition } from "remotion";
-import { Ranking } from "./MarioKart/Ranking/Ranking";
-import { Versus } from "./MarioKart/Versus/Versus";
-import { Rounds } from "./MarioKart/Rounds/Rounds";
+import {Composition} from 'remotion';
+import {Ranking} from './MarioKart/Ranking/Ranking';
+import {Versus} from './MarioKart/Versus/Versus';
+import {Rounds} from './MarioKart/Rounds/Rounds';
 
 // Each <Composition> is an entry in the sidebar!
 
 export const RemotionRoot: React.FC = () => {
-  return (
-    <>
-      <Composition
-        width={500}
-        height={1020}
-        id="Rounds"
-        fps={30}
-        durationInFrames={120}
-        component={Rounds}
-      />
-      <Composition
-        width={500}
-        height={500}
-        id="Versus"
-        fps={30}
-        durationInFrames={120}
-        component={Versus}
-      />
-      <Composition
-        id="MarioKart"
-        width={500}
-        height={1020}
-        fps={30}
-        durationInFrames={120}
-        component={Ranking}
-      />
-    </>
-  );
+	return (
+		<>
+			<Composition
+				id="Matches"
+				width={500}
+				height={1020}
+				fps={30}
+				durationInFrames={120}
+				component={Rounds}
+			/>
+			<Composition
+				id="Versus"
+				width={500}
+				height={500}
+				fps={30}
+				durationInFrames={120}
+				component={Versus}
+			/>
+			<Composition
+				id="Ranking"
+				width={500}
+				height={1020}
+				fps={30}
+				durationInFrames={120}
+				component={Ranking}
+			/>
+		</>
+	);
 };
