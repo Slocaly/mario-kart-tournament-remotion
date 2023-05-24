6bd2b78650c28f0406d0660673862efba5a08ce8
Audart Lucas <lucas.audart@zenika.com>
2023-05-24 21:52:37 +0200

    ⛳ update EndRound video

--- src/MarioKart/Rounds/Match.tsx
+++ src/MarioKart/Rounds/Match.tsx
@@ -1,88 +1,157 @@
 import {
-  Img,
-  spring,
-  staticFile,
-  useCurrentFrame,
-  useVideoConfig
-} from "remotion";
-import { loadFont } from "@remotion/google-fonts/VT323";
+	Img,
+	interpolate,
+	spring,
+	staticFile,
+	useCurrentFrame,
+	useVideoConfig,
+} from 'remotion';
+import {loadFont} from '@remotion/google-fonts/VT323';
+import {useMemo} from 'react';
 
 interface Player {
-  name: string;
-  photo: string;
+	name: string;
+	photo: string;
+	lose: boolean;
 }
 
 interface Players {
-  playerOne: Player;
-  playerTwo: Player;
-  index: number;
+	playerOne: Player;
+	playerTwo: Player;
 }
 
-export const Match: React.FC<Players> = ({ playerOne, playerTwo, index }: Players) => {
-  const frame = useCurrentFrame();
-  const { fps } = useVideoConfig();
-  const { fontFamily } = loadFont();
+interface MatchProps {
+	players: Players;
+	index: number;
+	animateOut?: boolean;
+}
 
-  const springAnim = spring({
-    frame: frame - index * 3,
-    fps,
-    from: -100,
-    to: 0,
-    durationInFrames: 30,
-    config: {
-      stiffness: 40
-    }
-  });
+export const Match: React.FC<MatchProps> = ({
+	players,
+	index,
+	animateOut = false,
+}) => {
+	const frame = useCurrentFrame();
+	const {fps} = useVideoConfig();
+	const {fontFamily} = loadFont();
 
-  return (
-    <div
-      style={{
-        width: "90%",
-        backgroundColor: "rgba(75,75,75, 0.5)",
-        fontSize: "2rem",
-        color: "white",
-        fontFamily,
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-        padding: "5px 40px 5px 20px",
-        transform: `translateX(${springAnim}%)`,
-        clipPath: "polygon(0 0, 100% 0%, 90% 100%, 0% 100%)"
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "20px"
-        }}
-      >
-        {playerOne.name}
-        <Img
-          src={staticFile(playerOne.photo)}
-          alt="photo"
-          style={{ width: "40px", objectFit: "contain" }}
-        />
-      </div>
-      <Img
-        src={staticFile("versus.png")}
-        alt="versus"
-        style={{ width: "30px", objectFit: "contain" }}
-      />
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "20px"
-        }}
-      >
-        <Img
-          src={staticFile(playerTwo.photo)}
-          alt="photo"
-          style={{ width: "40px", objectFit: "contain" }}
-        />
-        {playerTwo.name}
-      </div>
-    </div>
-  );
+	const {playerOne, playerTwo} = players;
+
+	const springAnimIn = spring({
+		frame: frame - index * 3,
+		fps,
+		from: -100,
+		to: 0,
+		durationInFrames: 20,
+		config: {
+			stiffness: 50,
+		},
+	});
+
+	const springAnimOut = spring({
+		frame: frame - index * 3 - 100,
+		fps,
+		from: 0,
+		to: 100,
+		durationInFrames: 20,
+		config: {
+			stiffness: 50,
+		},
+	});
+
+	const winAnim = spring({
+		frame: frame - 60,
+		fps,
+		from: 1,
+		to: 1.1,
+	});
+
+	const loseAnim = spring({
+		frame: frame - 60,
+		fps,
+		from: 1,
+		to: 0.7,
+	});
+
+	const opacityOnLose = interpolate(frame, [60, 70], [1, 0.5], {
+		extrapolateRight: 'clamp',
+	});
+
+	const grayscaleOnLose = interpolate(frame, [60, 70], [0, 1], {
+		extrapolateLeft: 'clamp',
+		extrapolateRight: 'clamp',
+	});
+
+	const springAnim = useMemo(
+		() => (animateOut ? springAnimIn - springAnimOut : springAnimIn),
+		[animateOut, springAnimIn, springAnimOut]
+	);
+
+	return (
+		<div
+			style={{
+				width: '90%',
+				backgroundColor: 'rgba(75,75,75, 0.5)',
+				fontSize: '2rem',
+				color: 'white',
+				fontFamily,
+				display: 'flex',
+				justifyContent: 'space-around',
+				alignItems: 'center',
+				padding: '5px 40px 5px 20px',
+				transform: `translateX(${springAnim}%)`,
+				clipPath: 'polygon(0 0, 100% 0%, 90% 100%, 0% 100%)',
+			}}
+		>
+			<div
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+					gap: '20px',
+				}}
+			>
+				{playerOne.name}
+				<Img
+					src={staticFile(playerOne.photo)}
+					alt="photo"
+					style={{
+						width: '40px',
+						objectFit: 'contain',
+						opacity: playerOne.lose ? opacityOnLose : 1,
+						filter: `grayscale(${playerOne.lose ? grayscaleOnLose : 0})`,
+						transform: `scale(${
+							playerOne.lose ? loseAnim : playerOne.lose === null ? 1 : winAnim
+						})`,
+					}}
+				/>
+			</div>
+			<Img
+				src={staticFile('versus.png')}
+				alt="versus"
+				style={{width: '30px', objectFit: 'contain'}}
+			/>
+			<div
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+					gap: '20px',
+				}}
+			>
+				<Img
+					src={staticFile(playerTwo.photo)}
+					alt="photo"
+					style={{
+						width: '40px',
+						objectFit: 'contain',
+						opacity: playerTwo.lose ? opacityOnLose : 1,
+						filter: `grayscale(${playerTwo.lose ? grayscaleOnLose : 0})`,
+						transform: `scale(${
+							playerTwo.lose ? loseAnim : playerTwo.lose === null ? 1 : winAnim
+						})`,
+					}}
+				/>
+				{playerTwo.name}
+			</div>
+		</div>
+	);
 };
--- src/MarioKart/Rounds/Rounds.tsx
+++ src/MarioKart/Rounds/Rounds.tsx
@@ -1,70 +1,105 @@
-import { AbsoluteFill, Img } from "remotion";
-import Matches0 from "../data/matches/matches0.json";
-import Matches1 from "../data/matches/matches1.json";
-import Matches2 from "../data/matches/matches2.json";
-import Matches3 from "../data/matches/matches3.json";
-import { Match } from "./Match";
-import { loadFont } from "@remotion/google-fonts/VT323";
-import { useEffect } from "react";
+import Matches0 from '../data/matches/matches0.json';
+import Matches1 from '../data/matches/matches1.json';
+import Matches2 from '../data/matches/matches2.json';
+import Matches3 from '../data/matches/matches3.json';
+import Matches4 from '../data/matches/matches4.json';
+import {
+	AbsoluteFill,
+	Img,
+	spring,
+	useCurrentFrame,
+	useVideoConfig,
+} from 'remotion';
+import {Match} from './Match';
+import {loadFont} from '@remotion/google-fonts/VT323';
+import {useMemo} from 'react';
 
-const { fontFamily } = loadFont();
-const matches = [Matches0, Matches1, Matches2, Matches3];
+const {fontFamily} = loadFont();
+const matches = [Matches0, Matches1, Matches2, Matches3, Matches4];
 
 interface RoundsProps {
-  round: number;
+	round: number;
+	animateOut?: boolean;
 }
 
-const getMatches = (roundNumber: number) => (matches[roundNumber]);
+const getMatches = (roundNumber: number) => matches[roundNumber];
 
-export const Rounds: React.FC<RoundsProps> = ({ round }: RoundsProps) => {
-  return (
-    <>
-      <AbsoluteFill>
-        <Img
-          src="https://i.pinimg.com/originals/d8/e7/e7/d8e7e7c2dac347163cd04af4e7db56a5.png"
-          alt="background"
-          style={{
-            objectFit: "cover",
-            height: "100%",
-            scale: "1.6"
-          }}
-        />
-      </AbsoluteFill>
-      <AbsoluteFill>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            padding: "0 10px"
-          }}
-        >
-          <h1
-            style={{
-              width: "100%",
-              textAlign: "center",
-              fontFamily,
-              fontSize: "4rem",
-              margin: "1.5rem 0"
-            }}
-          >
-            ROUND { round + 1 }
-          </h1>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-evenly",
-            height: "100%"
-          }}
-        >
-          {getMatches(round).map((round, i) => (
-            <Match key={i} {...round} index={i} />
-          ))}
-        </div>
-      </AbsoluteFill>
-    </>
+export const Rounds: React.FC<RoundsProps> = ({round, animateOut = false}) => {
+	const frame = useCurrentFrame();
+	const {fps} = useVideoConfig();
 
-  );
+	const springAnimIn = spring({
+		frame,
+		fps,
+		from: -100,
+		to: 0,
+	});
+
+	const springAnimOut = spring({
+		frame: frame - 145,
+		fps,
+		from: 0,
+		to: -100,
+	});
+
+	const springAnim = useMemo(
+		() => (animateOut ? springAnimIn + springAnimOut : springAnimIn),
+		[animateOut, springAnimIn, springAnimOut]
+	);
+
+	return (
+		<>
+			<AbsoluteFill>
+				<Img
+					src="https://i.pinimg.com/originals/d8/e7/e7/d8e7e7c2dac347163cd04af4e7db56a5.png"
+					alt="background"
+					style={{
+						objectFit: 'cover',
+						height: '100%',
+						scale: '1.6',
+					}}
+				/>
+			</AbsoluteFill>
+			<AbsoluteFill>
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'space-between',
+						alignItems: 'center',
+						padding: '0 10px',
+						transform: `translateY(${springAnim}px)`,
+					}}
+				>
+					<h1
+						style={{
+							width: '100%',
+							textAlign: 'center',
+							fontFamily,
+							fontSize: '4rem',
+							margin: '1.5rem 0',
+						}}
+					>
+						ROUND {round + 1}
+					</h1>
+				</div>
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'space-evenly',
+						height: '100%',
+					}}
+				>
+					{getMatches(round).map((currentRound, i) => (
+						<Match
+							key={i}
+							players={currentRound}
+							index={i}
+							animateOut={animateOut}
+						/>
+					))}
+				</div>
+			</AbsoluteFill>
+		</>
+	);
 };
